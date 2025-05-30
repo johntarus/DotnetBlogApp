c617eb64c2264bde6f8a8c8459eb4f7ba7ca2ff8
johntarus <tarusjohn96@gmail.com>
2025-05-30 10:56:38 +0300

    Updated exception and error handling to global scope

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,53 +9,32 @@ namespace BlogApp.Controllers;
 
 [ApiController]
 [Route("api/users")]
-public class UsersController(DatabaseContext context, IAuthService authService, IConfiguration config) : ControllerBase
+public class UsersController(IAuthService authService, IConfiguration config) : ControllerBase
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserResponseDto>> Register([FromBody] RegisterRequestDto request)
     {
-        try
-        {
             var user = await authService.RegisterAsync(request);
             if (ModelState.IsValid == false) return BadRequest(ModelState);
             return Ok(user);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
     }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserResponseDto>> Login([FromBody] LoginRequestDto request)
     {
-        try
-        {
             var user = await authService.LoginAsync(request);
             if (ModelState.IsValid == false) return BadRequest(ModelState);
             return Ok(user);
-        }
-        catch (Exception e)
-        {
-            return Unauthorized(e.Message);
-        }
     }
 
     [Authorize]
     [HttpGet("profile")]
     public async Task<ActionResult<ProfileResponseDto>> GetProfile()
     {
-        try
-        {
             var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
             var user = await authService.GetProfileAsync(userId);
             if (user == null) return NotFound();
             return Ok(user);
-        }
-        catch (Exception e)
-        {
-            return NotFound(e.Message);
-        }
     }
 
     [Authorize]
